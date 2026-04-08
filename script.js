8883e71a56b6d583c5e7fcc0aafde49dfb757cec
Tanishka2301 <sharmatanishka23012006@gmail.com>
2026-04-08 15:36:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // Floating hearts
 const container = document.getElementById("hearts");
 
-for (let i = 0; i < 20; i++) {
+for (let i = 0; i < 25; i++) {
     let heart = document.createElement("div");
     heart.className = "heart";
-    heart.innerHTML = "💗";
+    heart.innerHTML = "💜";
     heart.style.left = Math.random() * 100 + "vw";
     heart.style.fontSize = (Math.random() * 20 + 10) + "px";
     heart.style.animationDuration = (Math.random() * 5 + 3) + "s";
